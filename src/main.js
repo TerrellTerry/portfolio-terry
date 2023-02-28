73026db5f6aee3d120cdefda74ce2c81767c678a
Happychoco5 <theendereye@yahoo.com>
2023-02-28 00:31:52 -0800

    Updated some more things

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const introPageText = document.getElementById("introPageText");
 const softwareButton = document.getElementById("softwares-button");
 const gameButton = document.getElementById("games-button");
 
+const softwareProjects = document.getElementsByClassName("software-box");
+const gameProjects = document.getElementsByClassName("game-box");
+
 const chooseAdventureDiv = document.getElementById("chooseAdventure")
 
 let clickedButton = false;
@@ -17,6 +20,9 @@ document.addEventListener("click", event =>{
             console.log("Clicked software button.");
             clickedButton = true;
             fadeOut(gameButton, 0.1);
+            for(item of gameProjects){
+                item.style.display = 'none';
+            }
             setTimeout(fadeOut, 1000, chooseAdventureDiv, 0.1);
             setTimeout(bodyOverflow, 1000);
         }
@@ -24,12 +30,35 @@ document.addEventListener("click", event =>{
             console.log("Clicked games button.");
             clickedButton = true;
             fadeOut(softwareButton, 0.1);
+            for(item of softwareProjects){
+                item.style.display = 'none';
+            }
             //This is where we set up the rest of the logic needed to show the information about software/games
             setTimeout(fadeOut, 1000, chooseAdventureDiv, 0.1);
             setTimeout(bodyOverflow, 1000);
         }
     }
+
+    if(element.id == "nav-work-button"){
+        resetAdventure(0.1);
+    }
 });
+
+function resetAdventure(steps){
+    clickedButton = false;
+    fadeIn(chooseAdventureDiv, steps);
+    fadeIn(gameButton, steps);
+    fadeIn(softwareButton, steps);
+    setTimeout(makeProjectsVisible, 500);
+}
+function makeProjectsVisible(){
+    for(item of gameProjects){
+        item.style.display = 'inline';
+    }
+    for(item of softwareProjects){
+        item.style.display = 'inline';
+    }
+}
 
 function bodyOverflow(){
     document.body.style.overflow = 'visible';
